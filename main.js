ea23ea13b92ff2586f9471e6228b7ee04c655948
Adam <awhitehurst4@gmail.com>
2017-12-29 21:00:14 -0500

    angular

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-function mathhammer () {
+function myFunction() {
 
-  var name = document.getElementById('name').value
-  var save = document.getElementById('save').value
-  var bs = document.getElementById('bs').value
-  var attacks = document.getElementById('attacks').value
-  var strength = document.getElementById('strength').value
-  var ap = document.getElementById('ap').value
-  var d = document.getElementById('d').value
-  var points = document.getElementById('points').value
+  var name = document.getElementsByClassName('name').value;
+  var save = document.getElementsByClassName('save').value;
+  var bs = document.getElementsByClassName('bs').value;
+  var attacks = document.getElementsByClassName('attacks').value;
+  var strength = document.getElementsByClassName('strength').value;
+  var ap = document.getElementsByClassName('ap').value;
+  var d = document.getElementsByClassName('d').value;
+  var points = document.getElementsByClassName('points').value;
 
-  var hits = attacks*bs
-  var wounds = hits*0.5
-  var unsaved = wounds*(1-save)
-  var damage = unsaved*d
-  var dpp = damage/points
+  var hits = attacks*bs;
+  var wounds = hits*0.5;
+  var unsaved = wounds*(1-save);
+  var damage = unsaved*d;
+  var dpp = damage/points;
 
-  document.getElementById('model').innerHTML = name
-  document.getElementById('hits').innerHTML = hits
-  document.getElementById('wounds').innerHTML = wounds
-  document.getElementById('unsaved').innerHTML = unsaved
-  document.getElementById('damage').innerHTML = damage
-  document.getElementById('dpp').innerHTML = dpp
+  document.getElementsByClassName('hits').innerHTML = hits;
+  document.getElementsByClassName('wounds').innerHTML = wounds;
+  document.getElementsByClassName('unsaved').innerHTML = unsaved;
+  document.getElementsByClassName('damage').innerHTML = damage;
+  document.getElementsByClassName('dpp').innerHTML = dpp;
+
+    document.getElementById("demo").innerHTML = "Hello World";
 }
