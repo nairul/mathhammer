1f9e4b7beea9324126168d3a82cb2aa4623755af
Adam <awhitehurst4@gmail.com>
2018-01-01 17:06:26 -0500

    inserting

--- angular-test/main.js
+++ angular-test/main.js
@@ -1,19 +1,7 @@
-var app = angular.module('addDivApp', []);
-
-app.controller('addDivController', function($scope) {
-
-});
-
-app.directive('addDivDirective', function() {
-  return {
-    restrict: 'A',
-    scope: true,
-    template: '<button id="addDiv" class="btn btn-default" ng-click="click()">Add</button>',
-    controller: function($scope, $element, $compile) {
-      $scope.clicked = 0;
-      $scope.click = function() {
-        $('body').append($compile($('.form-group').clone())($scope));
-      }
+angular
+.module("myApp", [])
+.controller('ctrl',['$scope', function ($scope) {
+    $scope.logId = function() {
+        console.log($scope.id);
     }
-  }
-});
+}]);
