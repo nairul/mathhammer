4cd287e99a1fc78825f123f0c290d159e15e1150
Adam <awhitehurst4@gmail.com>
2018-01-02 12:20:59 -0500

    models  initialize with values to reduce console errors

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ angular
 
     ctrl.displayedModels = [];
     ctrl.models = [];
+ 
+ $scope.number = 0
+ $scope.name = 'Model'
+ $scope.attacks = 6
+ $scope.bs = 0.5
+ $scope.save = 0.5
+ $scope.ap = -0.16
+ $scope.d = 2
+ $scope.points = 10
+
 
     ctrl.addModel = function() {
       ctrl.displayedModels.push(model);
       ctrl.models.push({name: '', attacks: ''})
-
       $scope.calc = function(number,name,attacks,bs,save,ap,d,points) {  
         let hits = attacks*bs
         let woundst4 = hits*0.5
@@ -26,9 +35,10 @@ angular
         $scope.damage = damage
         $scope.dpp = dpp
 
-// "number" variable is a temporary solution. Users should not have to input the index number of every model. I want a unique index number to generate automatically for each model that is made.
         ctrl.models[number].name = name
         ctrl.models[number].attacks = attacks
+
+        // number variable is a temporary solution. Users should not have to input the index number of every model. I want a unique index number to generate automatically for each model that is made.
       } 
     }
   };
