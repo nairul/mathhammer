7f21742426f7827920725e7622217d9cce2f9ce2
Adam <awhitehurst4@gmail.com>
2018-01-01 23:41:01 -0500

    finished basic logic

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 
 # App TO DO
   ~~1) Target user input values in app.js~~
-  2) Add simple calc functions to js (Hits, Wounds, Damage, DPP). Remove all calcs from html.
-  3) Familiarize myself with graphs
+  ~~2) Add simple calc functions to js (Hits, Wounds, Damage, DPP). Remove all calcs from html~~
+  3) Familiarize myself with graphs. How do we access data from specific models?
   4) Add advanced options - rerolls & modifiers
   5) Add model-grouping functionality
   6) Wireframe
   7) Add CSS and Bootstrap
   8) Implement Graphs
-  9) Move model html template to seperate file 
+  ~~9) Move model html template to seperate file~~ 
 
 # Roadmap
   1) Correct formulas in spreadsheet and test (see above section)
--- app.js
+++ app.js
@@ -12,14 +12,17 @@ angular
     ctrl.addModel = function(modelIndex) {
       ctrl.displayedModels.push(models[modelIndex]
         );
-      $scope.hits = function(attacks,bs) {
-      hits = attacks*bs
-      return hits      
-      }
-      $scope.wounds = function() {
-      wounds = hits*0.5
-      return wounds
-      }
-    }
-    
+      $scope.calc = function(attacks,bs,save,ap,d,points) {
+      let hits = attacks*bs
+      let woundst4 = hits*0.5
+      let unsaved = woundst4*(1-(save+ap))
+      let damage = unsaved*d
+      let dpp = d/points
+      $scope.hits = hits
+      $scope.woundst4 = woundst4
+      $scope.unsaved = unsaved
+      $scope.damage = damage
+      $scope.dpp = dpp      
+    } 
+  }
   };
