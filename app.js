53d8c631e993b76b6132e9ed0fc48d8e897f34c6
Adam <awhitehurst4@gmail.com>
2018-01-02 22:40:27 -0500

    arrays in initial model working

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ window.app = angular
   $scope.models = [];
 
   // initialize models with starting values to reduce console errors
+  // var initialModel = {
+  //   name: 'Model',
+  //   attacks: 6,
+  //   bs: 0.5,
+  //   strength: 4,
+  //   save: 0.5,
+  //   ap: 0,
+  //   d: 1,
+  //   points: 10,
+  //   damageT3: 0,
+  //   damageT4: 0,
+  //   damageT5: 0,
+  //   damageT6: 0,
+  //   damageT7: 0,
+  //   damageT8: 0,
+  //   dppT3: 0,
+  //   dppT4: 0,
+  //   dppT5: 0,
+  //   dppT6: 0,
+  //   dppT7: 0,
+  //   dppT8: 0
+  // };
   var initialModel = {
     name: 'Model',
     attacks: 6,
@@ -16,20 +38,9 @@ window.app = angular
     ap: 0,
     d: 1,
     points: 10,
-    damageT3: 0,
-    damageT4: 0,
-    damageT5: 0,
-    damageT6: 0,
-    damageT7: 0,
-    damageT8: 0,
-    dppT3: 0,
-    dppT4: 0,
-    dppT5: 0,
-    dppT6: 0,
-    dppT7: 0,
-    dppT8: 0
+    damage: [],
+    dpp: []
   };
-
   $scope.addModel = function() {
     //add new model object to array of all models
     $scope.models.push(Object.assign({}, initialModel));
