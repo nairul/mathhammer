7ed010eec75d61c45b40d3b9a718be08425c37cd
Adam Whitehurst <awhitehurst4@gmail.com>
2018-01-04 13:20:33 -0500

    don't need vincents help

--- app.js
+++ app.js
@@ -5,35 +5,52 @@ window.app = angular
 
 
   // array of all models so I can access specific models later. 
-  $scope.models = [{
-  // start with 1 model on the page
-    name: 'Model 1',
-    attacks: 6,
-    bs: 0.5,
-    strength: 4,
-    save: 0.5,
-    ap: 0,
-    d: 1,
-    points: 10,
-    damage: [],
-    dpp: []
-  }];
+  $scope.models = [];
 
-  var newModel = {
-    name: 'Model' + ' ' + $scope.models.length, // why doesn't this work
-    attacks: 6,
-    bs: 0.5,
-    strength: 4,
-    save: 0.5,
-    ap: 0,
-    d: 1,
-    points: 10,
-    damage: [],
-    dpp: []
-  };
+  //chart
+  function updateGraph() {
+    var chartData = []
 
+    for (var i=0;i<$scope.models.length;i++){
+      chartData.push({
+        label: $scope.models[i].name,
+        backgroundColor: 'rgba(255, 255, 255, .4)', 
+        borderColor: 'rgb(255, 99, 132)', 
+        data: $scope.models[i].damage
+      })
+    }
+    var damageCanvas = document.getElementById("damageChart").getContext('2d');
+    var damageChart = new Chart(damageCanvas, {
+      type: 'line',
+      data: {
+        labels: ["T3", "T4", "T5", "T6", "T7", "T8"],
+        datasets: chartData,
+      },
+      options: {} 
+      })
+  }
+
+  //initial run
+  updateGraph();
+  //
+  $scope.$watch('models', updateGraph, true);
+
+  //add new model object to array of all models
   $scope.addModel = function() {
-    //add new model object to array of all models
+
+    var newModel = {
+      name: 'Model' + ' ' + ($scope.models.length+1),
+      attacks: 6,
+      bs: 0.5,
+      strength: 4,
+      save: 0.5,
+      ap: 0,
+      d: 1,
+      points: 10,
+      damage: [],
+      dpp: []
+    };
+
     $scope.models.push(Object.assign({}, newModel))
   };
 
---
+++ charts.directive.js
@@ -0,0 +1,7 @@
+app.directive('Charts', function() {
+  return {
+    restrict: 'E',
+    template: '<canvas id="damageChart" width="400" height="400"></canvas>',
+	}
+
+})
